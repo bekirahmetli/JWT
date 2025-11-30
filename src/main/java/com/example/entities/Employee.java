cd7309f5156075b141be5c46c24442c9dfa3740b
bekirahmetli <bekirahmetli1905@gmail.com>
2025-11-30 23:30:22 +0300

    feat(entity): add Employee entity with department relation

---
+++ src/main/java/com/example/entities/Employee.java
@@ -0,0 +1,28 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
+    @JoinColumn(name = "employee_department_id",referencedColumnName = "department_id")
+    private Department department;
+}
