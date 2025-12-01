f8815505e08d5ccaf83852c9b7b4cfb320c881cc
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-01 23:40:42 +0300

    feat(repository): add UserRepo with findByUsername method

---
+++ src/main/java/com/example/dao/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.dao;
+
+import com.example.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User,Long> {
+    Optional<User> findByUsername(String username); //Kullanıcı adını baz alarak kullanıcıyı arar
+}
