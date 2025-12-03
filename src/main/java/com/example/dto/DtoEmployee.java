26fdee7ca805bf53ba6dcd2a81d09006492242c9
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-03 20:16:12 +0300

    fix(dto): correct fistName to firstName in DtoEmployee

--- src/main/java/com/example/dto/DtoEmployee.java
+++ src/main/java/com/example/dto/DtoEmployee.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DtoEmployee {
     private Long id;
-    private String fistName;
+    private String firstName;
     private String lastName;
     private DtoDepartment department;
 }
